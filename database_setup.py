3d8024fce30c7bd7f6cec39b8acbb506450a9f9b
Alex <alexander.dudko@eufh.de>
2018-07-22 09:11:42 +0200

    add google login

--- database_setup.py
+++ database_setup.py
@@ -7,11 +7,22 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
+
+
 class Category(Base):
     __tablename__ = 'Category'
 
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
 
     @property
@@ -30,6 +41,8 @@ class Items(Base):
     price = Column(String(8))
     category_id = Column(Integer, ForeignKey('Category.id'))
     cagegory = relationship(Category)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
 
     @property
--- lotsofitems.py
+++ lotsofitems.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from database_setup import Base, Category, Items
+from database_setup import Base, Category, Items, User
 
 # Create database and create a shortcut for easier to update database
 engine = create_engine('sqlite:///item_catalog.db')
@@ -8,12 +8,17 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-category1 = Category(name="Indoor Furniture")
+category1 = Category(user_id=1, name="Indoor Furniture")
 session.add(category1)
 session.commit()
 
-items1 = Items(name="Livingroom Sofa", description="Perfect Sofa for your livingroom",
+items1 = Items(user_id=1, name="Livingroom Sofa", description="Perfect Sofa for your livingroom",
                 price="399", category_id=2)
+
+user1 = User(name="Alexander Dudko", email="alexander.dudko@eufh.de", picture='https://scontent-ams3-1.xx.fbcdn.net/v/t1.0-9/390888_280097078716240_2049534995_n.jpg?_nc_cat=0&oh=d149bbd571d0000f9551bdc17b180407&oe=5BDD1FCF')
+session.add(user1)
+session.commit()
+
 session.add(items1)
 session.commit()
 
